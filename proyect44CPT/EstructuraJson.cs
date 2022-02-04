510d5a0289238161a8432c1bb7af51bc1c3c9250
Carlos <carloseugenio024@gmail.com>
2022-02-04 12:42:27 -0600

    Agregado de Clase para los atributos del la peticion post

---
+++ proyect44CPT/EstructuraJson.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyect44CPT
+{
+    public class EstructuraJson
+    {
+        
+            public CarrierIdentifier carrierIdentifier { get; set; }
+            public IList<ShipmentIdentifiers> shipmentIdentifiers { get; set; }
+            public double latitude { get; set; }
+            public double longitude { get; set; }
+            public string utcTimestamp { get; set; }
+            public IList<ShipmentStops> shipmentStops { get; set; }
+            public double latestTemperature { get; set; }
+            public string latestTemperatureUnit { get; set; }
+            public int hoursOfServiceRemaining { get; set; }
+            public string customerId { get; set; }
+
+        
+        
+    }
+    public class CarrierIdentifier
+    {
+        public string type { get; set; }
+        public string value { get; set; }
+
+    }
+    public class ShipmentIdentifiers
+    {
+        public string value { get; set; }
+        public string type { get; set; }
+
+    }
+    public class AppointmentWindow
+    {
+        public string startDateTime { get; set; }
+        public string endDateTime { get; set; }
+        public string localTimeZoneIdentifier { get; set; }
+
+    }
+    public class Address
+    {
+        public string postalCode { get; set; }
+        public IList<string> addressLines { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string country { get; set; }
+
+    }
+    public class Contact
+    {
+        public string companyName { get; set; }
+        public string contactName { get; set; }
+        public string phoneNumber { get; set; }
+        public string phoneNumberCountryCode { get; set; }
+        public string phoneNumber2 { get; set; }
+        public string phoneNumber2CountryCode { get; set; }
+        public string email { get; set; }
+        public string faxNumber { get; set; }
+        public string faxNumberCountryCode { get; set; }
+
+    }
+    public class ShipmentStops
+    {
+        public int stopNumber { get; set; }
+        public AppointmentWindow appointmentWindow { get; set; }
+        public Address address { get; set; }
+        public Contact contact { get; set; }
+        public string stopName { get; set; }
+        public string arrivalDateTime { get; set; }
+        public string departureDateTime { get; set; }
+
+    }
+}
